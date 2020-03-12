c25eb76914bed49660af9b7f19cf85a055a22df2
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-03-12 12:47:15 -0300

    overflow

--- js/main.js
+++ js/main.js
@@ -8,16 +8,6 @@ anime({
   loop: false
 });
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vw = window.innerWidth * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vw', `${vw}px`);
-
 const staggerVisualizerEl = document.querySelector('.stagger-visualizer');
 const fragment = document.createDocumentFragment();
 const grid = [17, 30];
